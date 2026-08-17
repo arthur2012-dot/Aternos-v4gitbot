d9df7c6ac39a253098742b9e0b46e036aa4e784a
arthur2012-dot <arthurblucas@gmail.com>
2026-08-17 16:33:57 -0300

    Add main.js, Dockerfile, defaults, gitignore

---
+++ main.js
@@ -0,0 +1,60 @@
+import * as Mindcraft from './src/mindcraft/mindcraft.js';
+import settings from './settings.js';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+import { readFileSync } from 'fs';
+
+function parseArguments() {
+    return yargs(hideBin(process.argv))
+        .option('profiles', {
+            type: 'array',
+            describe: 'List of agent profile paths',
+        })
+        .option('task_path', {
+            type: 'string',
+            describe: 'Path to task file to execute'
+        })
+        .option('task_id', {
+            type: 'string',
+            describe: 'Task ID to execute'
+        })
+        .help()
+        .alias('help', 'h')
+        .parse();
+}
+const args = parseArguments();
+if (args.profiles) {
+    settings.profiles = args.profiles;
+}
+if (args.task_path) {
+    let tasks = JSON.parse(readFileSync(args.task_path, 'utf8'));
+    if (args.task_id) {
+        settings.task = tasks[args.task_id];
+        settings.task.task_id = args.task_id;
+    }
+    else {
+        throw new Error('task_id is required when task_path is provided');
+    }
+}
+
+// these environment variables override certain settings
+if (process.env.MINECRAFT_PORT) {
+    settings.port = process.env.MINECRAFT_PORT;
+}
+if (process.env.MINDSERVER_PORT) {
+    settings.mindserver_port = process.env.MINDSERVER_PORT;
+}
+if (process.env.PROFILES && JSON.parse(process.env.PROFILES).length > 0) {
+    settings.profiles = JSON.parse(process.env.PROFILES);
+}
+if (process.env.INSECURE_CODING) {
+    settings.allow_insecure_coding = process.env.INSECURE_CODING === 'true';
+}
+if (process.env.HOST) {
+    settings.host = process.env.HOST;
+}
+if (process.env.GROQCLOUD_API_KEY) {
+    // key can also come from env on Railway
+}
+
+Mindcraft.init(settings);
